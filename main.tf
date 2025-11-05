11b5f848e9acfdf8c7c30df14017f65d8330968d
Atul Kamble <atul_kamble@hotmail.com>
2025-11-05 16:03:53 +0530

    code

--- main.tf
+++ main.tf
@@ -32,6 +32,13 @@ resource "aws_security_group" "jenkins_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 80
     to_port     = 80
